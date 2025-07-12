218d4b91b6380a39f7db4989dd1ab8bfe68a957f
Shakti Lucidia <shakti_lucidia@proton.me>
2025-07-12 02:29:17 +0200

    Able to parce XBM file

--- FontGenerator/FontGenerator/Models/XbmFile.cs
+++ FontGenerator/FontGenerator/Models/XbmFile.cs
@@ -8,10 +8,24 @@ namespace FontGenerator.Models;
 public class XbmFile
 {
     /// <summary>
-    /// Dataset name
+    /// Dataset name regexp
     /// </summary>
     private const string DatasetNameRegexpText = @"(?'dataset'\S*)_bits\[\] = {";
+
+    /// <summary>
+    /// Image width regexp template
+    /// </summary>
+    private const string WidthRegexpTemplate = "#define {0}_width (?'width'\\d+)";
     
+    /// <summary>
+    /// Image height regexp template
+    /// </summary>
+    private const string HeightRegexpTemplate = "#define {0}_height (?'height'\\d+)";
+    
+    /// <summary>
+    /// Image data regexp template
+    /// </summary>
+    private const string DataRegexpTemplate = @"static char {0}_bits\[\] = {(?'data'.+)};";
     
     private static readonly Regex DatasetNameRegexp = new Regex(DatasetNameRegexpText, RegexOptions.Compiled | RegexOptions.IgnoreCase);
     
@@ -72,19 +86,92 @@ public class XbmFile
     {
         var content = File.ReadAllText(path);
         
-        var matches = DatasetNameRegexp.Matches(content);
-        if (!matches.Any())
+        var datasetNameMatches = DatasetNameRegexp.Matches(content);
+        if (!datasetNameMatches.Any())
         {
             throw new ArgumentException("Not a valid XBM file!", nameof(path));
         }
 
-        var datasetName = matches
+        #region Dataset name
+        
+        var datasetName = datasetNameMatches
             .First()
             .Groups["dataset"]
             .Captures
             .First()
             .Value;
+        
+        #endregion
+        
+        #region Width
+        
+        var widthRegexp = new Regex(String.Format(WidthRegexpTemplate, datasetName), RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        
+        var widthMatches = widthRegexp.Matches(content);
+        if (!widthMatches.Any())
+        {
+            throw new ArgumentException("Width is missing!", nameof(path));
+        }
+        
+        var width = int.Parse(widthMatches
+            .First()
+            .Groups["width"]
+            .Captures
+            .First()
+            .Value);
+        
+        #endregion
+        
+        #region Height
+        
+        var heightRegexp = new Regex(String.Format(HeightRegexpTemplate, datasetName), RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        
+        var heightMatches = heightRegexp.Matches(content);
+        if (!heightMatches.Any())
+        {
+            throw new ArgumentException("Height is missing!", nameof(path));
+        }
+        
+        var height = int.Parse(heightMatches
+            .First()
+            .Groups["height"]
+            .Captures
+            .First()
+            .Value);
+        
+        #endregion
+        
+        #region Data
+        
+        var dataRegexp = new Regex(DataRegexpTemplate.Replace("{0}", datasetName), RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        
+        var dataMatches = dataRegexp.Matches(content);
+        if (!dataMatches.Any())
+        {
+            throw new ArgumentException("Data is missing!", nameof(path));
+        }
+        
+        var dataStrings = dataMatches
+            .First()
+            .Groups["data"]
+            .Captures
+            .First()
+            .Value
+            .Split(", ")
+            .Select(s => s.Trim())
+            .ToList();
 
-        return new XbmFile(datasetName, 10, 10, new List<byte>());
+        if (dataStrings.Last() == string.Empty)
+        {
+            dataStrings.RemoveAt(dataStrings.Count - 1);
+        }
+
+        var data = dataStrings
+            .Select(s => Convert.ToByte(s, 16))
+            .ToList();
+        
+        #endregion
+
+        return new XbmFile(datasetName, width, height, data);
     }
 }
