4a737a3e59040f82d7110e88e4278293d6372707
Shakti Lucidia <shakti_lucidia@proton.me>
2025-07-15 01:01:39 +0200

    Able to generate FMGL font files

--- FontGenerator/FontGenerator/Program.cs
+++ FontGenerator/FontGenerator/Program.cs
@@ -7,21 +7,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        //var xbmFile = XbmFile.Parse(@"/mnt/storage0/shakti/projects/Electronics/FMGL Fonts/Extractor/fontforge/FreeSansExtracted/yericyrillic.xbm");
-
+        // Search for XBM images here
         var sourceDirectory = @"/mnt/storage0/shakti/projects/Electronics/FMGL Fonts/Extractor/fontforge/FreeSansExtracted/";
         
+        // Use this characters table
         var charactersTableFile = @"/mnt/storage0/shakti/projects/Electronics/STM32_L2HAL_FontsGenerator/Resources/KOI8-R.json";
         
-        /*var xbmFiles = Directory.EnumerateFiles(sourceDirectory, "*.xbm", SearchOption.AllDirectories);
-
-        var parsedXbmFiles = xbmFiles
-            .Select(f => XbmFile.Parse(f))
-            .ToList();*/
+        // Save result here
+        var resultFile = @"/mnt/storage0/shakti/projects/Electronics/FMGL Fonts/Extractor/fontforge/FreeSans.fmglfont";
         
         var charactersTable = JsonSerializer.Deserialize<CharactersTable>(File.ReadAllText(charactersTableFile));
 
         var missingFilesCount = 0;
+
+        var charactersDictionary = new Dictionary<int, XbmFile>();
 
         foreach (var character in charactersTable.Characters)
         {
@@ -34,9 +33,50 @@ class Program
                 continue;
             }
             
-            var xbmFile = XbmFile.Parse(fullPath);
+            charactersDictionary.Add(character.Code, XbmFile.Parse(fullPath));
         }
         
+        // FMGL Font structure
+        // [9 bytes]: 'FMGLFONT1'
+        // [4 bytes]: characters count
+        // []: characters, each character is:
+        // -- [4 bytes]: code
+        // -- [2 bytes]: width
+        // -- [2 bytes]: height
+        // -- [width * height bytes]: character data
+
+        using (var outputStream = new FileStream(resultFile, FileMode.Create))
+        {
+            // Signature
+            outputStream.WriteByte(0x46);
+            outputStream.WriteByte(0x4D);
+            outputStream.WriteByte(0x47);
+            outputStream.WriteByte(0x4C);
+            outputStream.WriteByte(0x46);
+            outputStream.WriteByte(0x4F);
+            outputStream.WriteByte(0x4E);
+            outputStream.WriteByte(0x54);
+            outputStream.WriteByte(0x31);
+            
+            // Characters count
+            outputStream.Write(BitConverter.GetBytes(charactersDictionary.Count));
+            
+            foreach (var character in charactersDictionary)
+            {
+                // Code
+                outputStream.Write(BitConverter.GetBytes(character.Key));
+                
+                // Width
+                outputStream.Write(BitConverter.GetBytes(character.Value.Width));
+                
+                // Height
+                outputStream.Write(BitConverter.GetBytes(character.Value.Height));
+                
+                // Data
+                outputStream.Write(character.Value.Data.ToArray());
+            }
+        }
+
         Console.WriteLine($"Missing files: {missingFilesCount}");
     }
 }
